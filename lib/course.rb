b913fb3790fc5db0ea37ed3869b44de47d9481b4
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-05 21:08:28 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,8 +1,15 @@
 class Course
   attr_accessor :title, :schedule, :description
   
+  def initialize
+    @@all << self
+  end
+  
   def self.all
     @@all
   end
+  
+  def self.reset_all
+    
 end
 
